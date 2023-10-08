47335d221bcd47d0b7f87ab284a43444556885e7
mrlijing <lijing16@yahoo.com>
2023-10-08 15:53:06 +1000

    Add hover effects to each folder frame

--- Report.py
+++ Report.py
@@ -3,8 +3,8 @@ from grid_frame import GridFrame
 
 class Report(tk.Frame):
     def __init__(self, parent):
-        super().__init__(parent)
-        self.max_col = 3
+        super().__init__(parent, borderwidth=20)
+        self.max_col = 4
         self.grid = GridFrame(self, self.max_col)
         self.cell_folder = {}
         self.empty_cells = []
@@ -23,15 +23,38 @@ class Report(tk.Frame):
 
         self.grid.pack(fill=tk.BOTH, expand=True)
 
-    def create_file(self):
+    def create_file(self, name):
+
+        def on_enter(e):
+            img_label.configure(cursor='hand2')
+            img_label.configure(bg='white')
+            name_label.configure(cursor='hand2')
+            name_label.configure(bg='white')
+
+        def on_leave(e):
+            img_label.configure(bg='lightgrey')
+            name_label.configure(bg='lightgrey')
 
         if len(self.empty_cells) == 0:
             self.create_grid()
 
         cell = self.empty_cells.pop(0)
-        img = tk.PhotoImage(file='folder.png')
-        img_label = tk.Label(cell, image=img, bg='lightgrey')
-        img_label.image = img
-        img_label.pack(padx=20, pady=20)
+        folder_frame = tk.Frame(cell)
 
-        self.cell_folder[cell] = img_label
+        img = tk.PhotoImage(file='folder.png')
+        img_label = tk.Label(folder_frame, image=img, bg='lightgrey')
+        img_label.image = img
+        img_label.pack(fill=tk.X)
+
+        name_label = tk.Label(folder_frame, bg='lightgrey', text=name)
+        name_label.pack(fill=tk.X)
+
+        folder_frame.bind("<Enter>", on_enter)
+        folder_frame.bind("<Leave>", on_leave)
+        folder_frame.pack(padx=20, pady=20)
+
+        self.cell_folder[cell] = folder_frame
+
+    def on_click(self, frame: tk.Frame):
+
+        None
